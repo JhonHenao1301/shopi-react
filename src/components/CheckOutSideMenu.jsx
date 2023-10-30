5dfd03c75aec480ad4d3eaef04338b9b06349120
Jhon Henao <henao.jhon@correounivalle.edu.co>
2023-10-30 10:19:48 -0500

    Total price setted

--- src/components/CheckOutSideMenu.jsx
+++ src/components/CheckOutSideMenu.jsx
@@ -2,10 +2,15 @@
 import { useContext } from "react"
 import { ShoppingCartContext } from "../Context"
 import { CloseMenuIcon } from "../assets/Icons/Icons"
+import { totalPrice } from "../utils/index"
 import OrderCard from "./OrderCard"
 
 export default function CheckOutSideMenu () {
     const { cart, isOpenCheckout } = useContext(ShoppingCartContext)
+    const total = (products) => {
+        return totalPrice(products)
+    }
+
 
     return ( 
         <aside className={`${isOpenCheckout ? 'flex' : 'hidden'} checkout flex-col justify-between gap-4 fixed right-0 border border-black rounded-lg bg-gray-5 py-4 px-2`}>
@@ -27,7 +32,7 @@ export default function CheckOutSideMenu () {
             <div className="flex flex-col gap-2 px-2">
                 <p className="flex justify-between ">
                     <span>Total</span>
-                    <span className="font-semibold">$000</span>
+                    <span className="font-semibold">${total(cart)}</span>
                 </p>
                 <button className="border rounded-lg bg-gray-30 text-gray-5 p-2">
                     Checkout
